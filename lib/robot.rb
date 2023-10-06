b4846268962c67bdd74f538568c645a067dec135
Anna Cavalla <anna.cavalla@gmail.com>
2023-10-06 15:08:57 +0100

    Refactor move method to reduce complexity

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,5 +1,6 @@
 class Robot
   attr_reader :position, :direction
+  MOVEMENT = { N: [0, 1], S: [0,-1], E: [1,1], W: [1,-1] }
   def initialize(x, y, direction)
     self.place(x, y, direction)
   end
@@ -10,15 +11,6 @@ class Robot
   end
 
   def move
-    case @direction
-    when :N
-      @position[0] += 1
-    when :S
-      @position[0] -= 1
-    when :E
-      @position[1] += 1
-    when :W
-      @position[1] -= 1
-    end
+    @position[MOVEMENT[@direction][0]] += MOVEMENT[@direction][1]
   end
 end
