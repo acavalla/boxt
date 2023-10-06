82c43a29694bc670bef8e3fe3a156465957408ea
Anna Cavalla <anna.cavalla@gmail.com>
2023-10-06 17:07:26 +0100

    Add method for turning right, move allow_move into private section

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,27 +1,39 @@
 require "robot"
 
 describe 'Robot' do
-  it 'initialises in a position and facing a direction' do
-    robot = Robot.new(0,0,"NORTH")
-    expect(robot.position).to eq([0,0])
-    expect(robot.direction).to eq(:N)
+  context 'initialise' do
+    it 'initialises in a position and facing a direction' do
+      robot = Robot.new(0,0,"NORTH")
+      expect(robot.position).to eq([0,0])
+      expect(robot.direction).to eq(:N)
+    end
   end
 
-  it 'can move in a direction' do
-    robot = Robot.new(0,0,"NORTH")
-    robot.move()
-    expect(robot.position).to eq([1,0])
+  context 'move' do
+    it 'can move according to the direction it is facing' do
+      robot = Robot.new(0,0,"NORTH")
+      robot.move()
+      expect(robot.position).to eq([1,0])
+    end
+
+    it 'cannot fall off the table by moving south from x=0' do
+      robot = Robot.new(0,0,"SOUTH")
+      robot.move()
+      expect(robot.position).to eq([0,0])
+    end
+
+    it 'cannot fall off the table by moving east from x=5' do
+      robot = Robot.new(5,5,"EAST")
+      robot.move()
+      expect(robot.position).to eq([5,5])
+    end
   end
 
-  it 'cannot fall off the table by moving south from x=0' do
-    robot = Robot.new(0,0,"SOUTH")
-    robot.move()
-    expect(robot.position).to eq([0,0])
-  end
-
-  it 'cannot fall off the table by moving east from x=5' do
-    robot = Robot.new(5,5,"EAST")
-    robot.move()
-    expect(robot.position).to eq([5,5])
+  context 'right' do
+    it 'can turn right from south and will then be facing west' do
+      robot = Robot.new(0,0,"SOUTH")
+      robot.right()
+      expect(robot.direction).to eq(:W)
+    end
   end
 end
