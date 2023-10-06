047d215f19731249206d7eebb74a79987dfb740f
Anna Cavalla <anna.cavalla@gmail.com>
2023-10-06 14:57:44 +0100

    Add move command to robot

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,2 +1,24 @@
 class Robot
+  attr_reader :position, :direction
+  def initialize(x, y, direction)
+    self.place(x, y, direction)
+  end
+
+  def place(x, y, direction)
+    @position = [x, y]
+    @direction = direction[0].to_sym
+  end
+
+  def move
+    case @direction
+    when :N
+      @position[0] += 1
+    when :S
+      @position[0] -= 1
+    when :E
+      @position[1] += 1
+    when :W
+      @position[1] -= 1
+    end
+  end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,4 +1,15 @@
 require "robot"
 
 describe 'Robot' do
+  it 'initialises in a position and facing a direction' do
+    robot = Robot.new(0,0,"NORTH")
+    expect(robot.position).to eq([0,0])
+    expect(robot.direction).to eq(:N)
+  end
+
+  it 'can move in a direction' do
+    robot = Robot.new(0,0,"NORTH")
+    robot.move()
+    expect(robot.position).to eq([1,0])
+  end
 end
