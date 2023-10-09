ea3ce3623addc0cbfdcf8fdb4da5285826ca28a1
Anna Cavalla <anna.cavalla@gmail.com>
2023-10-09 18:23:50 +0100

    Fix edge cases of left and right turn commands and edge cases of moving beyond table limits

--- lib/robot.rb
+++ lib/robot.rb
@@ -2,7 +2,7 @@ class Robot
   attr_reader :position, :direction
   MOVEMENT = { N: [0, 1], S: [0,-1], E: [1,1], W: [1,-1] }
   LIMIT = 5
-  CARDINALS = [:N, :E, :S, :W]
+  CARDINALS = [:N, :E, :S, :W, :N, :W]
   def initialize(x, y, direction)
     self.place(x, y, direction)
   end
@@ -26,8 +26,10 @@ class Robot
 
   private
   def allow_move
-    position.each do | i |
-      if i == 0 && (direction == :S || direction == :W) || i == LIMIT && (direction == :N || direction == (:E))
+    new_position = position.map(&:clone)
+    new_position[MOVEMENT[direction][0]] += MOVEMENT[direction][1]
+    new_position.each do | i |
+      if i < 0 || i > 5
         return false
       end
     end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -16,16 +16,28 @@ describe 'Robot' do
       expect(robot.position).to eq([1,0])
     end
 
-    it 'cannot fall off the table by moving south from x=0' do
-      robot = Robot.new(0,0,"SOUTH")
+    it 'cannot fall off the table by moving north from x=5' do
+      robot = Robot.new(5,2,"NORTH")
       robot.move()
-      expect(robot.position).to eq([0,0])
+      expect(robot.position).to eq([5,2])
     end
 
-    it 'cannot fall off the table by moving east from x=5' do
-      robot = Robot.new(5,5,"EAST")
+    it 'cannot fall off the table by moving south from x=0' do
+      robot = Robot.new(0,2,"SOUTH")
       robot.move()
-      expect(robot.position).to eq([5,5])
+      expect(robot.position).to eq([0,2])
+    end
+
+    it 'cannot fall off the table by moving east from y=5' do
+      robot = Robot.new(2,5,"EAST")
+      robot.move()
+      expect(robot.position).to eq([2,5])
+    end
+
+    it 'cannot fall off the table by moving west from y=0' do
+      robot = Robot.new(2,0,"WEST")
+      robot.move()
+      expect(robot.position).to eq([2,0])
     end
   end
 
@@ -36,6 +48,14 @@ describe 'Robot' do
       expect(robot.direction).to eq(:W)
     end
 
+    it 'can go right from west and will then be facing north' do
+      robot = Robot.new(0,0,"WEST")
+      robot.right()
+      expect(robot.direction).to eq(:N)
+    end
+  end
+
+  context 'left' do
     it 'can turn left from south and will then be facing east' do
       robot = Robot.new(0,0,"SOUTH")
       robot.left()
